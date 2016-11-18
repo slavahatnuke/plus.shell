2825fe3247206b2084e969eeb95e08434aa5ad54
slava hatnuke <slava.hatnuke@gmail.com>
2016-11-19 00:27:37 +0200

    v1.0.12 right logs for errors

--- src/Commander.js
+++ src/Commander.js
@@ -25,7 +25,13 @@ module.exports = (app) => {
 
     program.command('run [command...]')
         .action((command) => app.Client.run(command.join(' ')).then(done, (err) => {
-            setTimeout(() => process.exit(1), 1000);
+            setTimeout(() => {
+                console.log('>', command.join(' '), '\n');
+                console.log('ERROR >');
+                console.log(err);
+                console.log('\n');
+                process.exit(1);
+            }, 1000);
         }));
 
     return program;
